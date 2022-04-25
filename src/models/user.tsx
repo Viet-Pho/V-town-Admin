cfa66875a32a323d7cf23f20f703bac1b8ecf234
n.q.hung <n.q.hung@azoom.jp>
2022-04-25 14:59:23 +0000

    :+1: Implement calling API method

---
+++ src/models/user.tsx
@@ -0,0 +1,5 @@
+import callApi from '../util/callApi'
+
+export const getUsers = async () => {
+    return callApi('GET', '/api/users/110/fetch?name=xxx', null)
+}
---
+++ src/util/callApi.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default (method, url, data) => {
+  return axios({
+    method: method,
+    url: `${url}`,
+    data: data,
+     headers : {
+        'Content-Type': 'application/json'
+    }
+  });
+}
