befd45f933893bd737130a662525fec27b0ffe15
n.q.hung <n.q.hung@azoom.jp>
2022-06-02 03:32:31 +0000

    :hammer: Change api /auth to get user from database

--- src/pages/api/auth.tsx
+++ src/pages/api/auth.tsx
@@ -1,13 +1,21 @@
 import jwtAuth from 'middleware/jwt';
+import database from '../../database';
 
-const auth = (req, res) => {
+const auth = async (req, res) => {
   const {method} = req;
   if (method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     return res.status(405).end(`Method ${method} Not Allowed`);
   }
+  const {id: userId} = req.user;
+  const [user] = await database('users')
+    .where('id', userId)
+    .where('is_deleted', 0)
+    .select('username', 'account_type as role', 'id', 'email', 'confirmed');
+  
+  if (!user) return res.status(404).end(`User ${userId} not found`);
 
-  return res.status(200).send(req.user);
+  return res.status(200).send(user);
 };
 
 export default jwtAuth(auth);
