9117e29b33a215b2ce499a59dfa54a8c35521f98
n-v-nam <namxuanhoapro@gmail.com>
2022-06-16 09:36:59 +0700

    :tada: Add feature calculator room price

---
+++ src/constants/room.tsx
@@ -0,0 +1,13 @@
+const milestoneHour = '07';
+
+const dayOfWeek = {
+  sunday: 0,
+  monday: 1,
+  tuesday: 2,
+  wendnesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+};
+
+export {milestoneHour, dayOfWeek};
